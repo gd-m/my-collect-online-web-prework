c75cdd8fee947003af9a39de554c165610b7c020
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-04 06:15:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  if block_given?
+    i=0
+    while i<array.length
+      yield(array[i])
+      i=i+1
+    end
+  else
+    "There is no block given"
+  end
+  array
+end
 
